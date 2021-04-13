19305e78537b3709c0f85cb0dcb4a9a6c323ef84
razikusman <razikusman@gmail.com>
2021-04-13 22:13:39 +0530

    Error Configured and fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import { HostListener } from '@angular/core';
+import { Scroll } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'LED';
+ 
+}
+
+
+// onWindowScroll(e) {
+//     let element = document.querySelector('.navbar');
+//     if (window.pageYOffset > element.clientHeight) {
+//       element.classList.add('navbar-inverse');
+//     } else {
+//       element.classList.remove('navbar-inverse');
+//     }
+//   }
+if (Scroll) {
+  
 }
